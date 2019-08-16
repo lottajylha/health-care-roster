5eeff8d18294c3a9ab09d771f462f4660d9be520
Lotta Jylhä <lotta_jylha@hotmail.com>
2019-08-16 23:00:48 +0300

    importteja puuttui

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
+import sys
+import logging
 
 app = Flask(__name__)
 
--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 from application import app
+import sys
+import logging
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
 if __name__ == '__main__':
     app.run(debug=True)
